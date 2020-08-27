6006fcefb6edf62b3a2c1afafd468998576284cb
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2020-08-27 19:50:39 +0100

    Updates

---
+++ precache-manifest.9e1df04e609e0115a7c463c9c5c5305c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c358efce20c5148387cc8f4d9a7d5433",
+    "url": "/openweatherapi/index.html"
+  },
+  {
+    "revision": "0e54d3dfb58caa3877d5",
+    "url": "/openweatherapi/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "8ecc987c8f3fe953b73a",
+    "url": "/openweatherapi/static/css/main.f04abc68.chunk.css"
+  },
+  {
+    "revision": "0e54d3dfb58caa3877d5",
+    "url": "/openweatherapi/static/js/2.93c5cfb5.chunk.js"
+  },
+  {
+    "revision": "b846b5b2fcb30d4eb0a413c317540089",
+    "url": "/openweatherapi/static/js/2.93c5cfb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecc987c8f3fe953b73a",
+    "url": "/openweatherapi/static/js/main.17872df2.chunk.js"
+  },
+  {
+    "revision": "45ae9fc8ed1c2b0154f0",
+    "url": "/openweatherapi/static/js/runtime-main.948f434d.js"
+  },
+  {
+    "revision": "29c7b80a1749c556b8f7327cedaa206c",
+    "url": "/openweatherapi/static/media/branding@2x.29c7b80a.png"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/openweatherapi/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/openweatherapi/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/openweatherapi/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/openweatherapi/static/media/owfont-regular.f3ce0ef1.svg"
+  }
+]);
